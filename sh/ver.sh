bf4becfb6191963668333006d40d2faa34550c46
i18n <i18n.site@gmail.com>
2024-02-02 03:07:35 +0800

    v0.1.82

--- sh/ver.sh
+++ sh/ver.sh
@@ -4,17 +4,17 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
-source conf.sh
-
-cp -f git.config ../.git/config
-
 ver=$(cargo metadata --no-deps --format-version=1 | jq -r '.packages[0].version')
 branch=$(git branch 2>/dev/null | sed -e '/^[^*]/d' | awk -F' ' '{print $2}')
+
+cp -f git.config ../.git/config
 
 cd /tmp
 echo $ver >v
 $DIR/rcp.sh v
-cd $DIR
+
+cd $(dirname $DIR)
+
 git branch -D v || true
 git switch --orphan v
 rm -rf *
