1f1af1573ea8db0fddf03fc312bd0589cc14339b
i18n <i18n.site@gmail.com>
2024-02-01 22:04:49 +0800

    🔥

---
+++ sh/cross.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+ROOT=${DIR%/*}
+cd $DIR
+set -ex
+
+source RUSTFLAGS.sh
+
+installed=$(rustup target list --installed)
+
+unameOut="$(uname -s)"
+
+target_list=$(rustup target list | awk '{print $1}')
+
+case "${unameOut}" in
+Linux)
+  docker pull i18nsite/x86_64-pc-windows-msvc-cross &
+  docker pull i18nsite/aarch64-pc-windows-msvc-cross &
+  TARGET_LI=$(echo "$target_list" | grep "\-linux-" | grep -E "x86|aarch64" | grep -E "[musl|gun]$" | grep -v "i686-unknown-linux-musl")
+  ;;
+Darwin) TARGET_LI=$(echo "$target_list" | grep "\-apple-" | grep -v "\-ios") ;;
+esac
+
+if ! command -v cargo-zigbuild &>/dev/null; then
+  cargo install cargo-zigbuild
+fi
+
+ver=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')
+
+build_mv() {
+  cargo zigbuild --release --target $1
+  $DIR/cross/mv.sh $ver $1
+}
+
+for target in ${TARGET_LI[@]}; do
+  (echo $installed | grep -q $target || rustup target add $target) && rustup update nightly
+  build_mv $target
+done
+
+wait
+if [ "$unameOut" == "Linux" ]; then
+  ./cross/build.sh
+fi
---
+++ sh/cross/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+ROOT=${DIR%/*}
+set -ex
+
+source ../RUSTFLAGS.sh
+
+if ! command -v cross &>/dev/null; then
+  cargo install cross
+fi
+
+ver=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')
+
+cs() {
+  cross build --target $1 --release
+  ./mv.sh $ver $1
+}
+
+STATIC="$RUSTFLAGS -Ctarget-feature=+crt-static"
+
+# CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_RUSTFLAGS="$RUSTFLAGS $STATIC"
+
+CARGO_TARGET_X86_64_PC_WINDOWS_MSVC_RUSTFLAGS=$STATIC
+CARGO_TARGET_AARCH64_PC_WINDOWS_MSVC_RUSTFLAGS=$STATIC
+
+for i in ${x86_64-pc-windows-msvc aarch64-pc-windows-msvc}; do
+  cs build $i
+done
