2fb4309f471d1fdace8223ed7a516616b7baf1b3
i18n <i18n.site@gmail.com>
2024-02-02 17:50:34 +0800

    v0.1.113

--- sh/dist/VER.sh
+++ sh/dist/VER.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-if ! [[ -v VER ]]; then
-  export VER=$(cargo metadata --format-version=1 --no-deps | jq -r '.workspace_default_members[0]' | awk -F@ '{print $2}')
-fi
+[[ -v VER ]] || export VER=$(cargo metadata --format-version=1 --no-deps | jq -r '.workspace_default_members[0]' | awk -F@ '{print $2}')
