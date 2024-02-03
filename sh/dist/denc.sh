86c7822a633004c13a484947c306c9ed139fe191
i18n <i18n.site@gmail.com>
2024-02-04 00:56:22 +0800

    v0.1.158

--- sh/dist/denc.sh
+++ sh/dist/denc.sh
@@ -4,9 +4,16 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
-name=conf
-tar=$name.tar.zstd
+fp=id_ed25519
 
-echo $ENC_PASSWD | gpg --batch --yes --passphrase-fd 0 --output $tar --decrypt $tar.enc
-tar --zstd -xvf $tar
-rm $tar
+if [ ! -f "$HOME/.ssh/$fp" ]; then
+  echo $ENC_PASSWD | gpg --batch --yes --passphrase-fd 0 --output $fp --decrypt $fp.enc
+  mkdir -p ~/.ssh
+  mv $fp ~/.ssh/
+  chmod 700 ~/.ssh
+  chmod 600 ~/.ssh/$fp
+fi
+
+if [ ! -d "conf" ]; then
+  git clone --depth=1 git@github.com:i18n-pri/bin.conf.git conf
+fi
--- sh/dist/enc.sh
+++ sh/dist/enc.sh
@@ -4,11 +4,6 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
-name=conf
-tar=$name.tar.zstd
+fp=id_ed25519
 
-rm -rf $tar
-
-ZSTD_CLEVEL=19 tar --zstd -cvpf $tar $name
-
-echo $ENC_PASSWD | gpg --batch --yes --passphrase-fd 0 --output $tar.enc --symmetric --cipher-algo AES256 $tar
+echo $ENC_PASSWD | gpg --batch --yes --passphrase-fd 0 --output $fp.enc --symmetric --cipher-algo AES256 $fp
